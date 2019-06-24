67a84312e472ac96933e0839159445a1598e78a0
zhs25427199 <zhs25427199@gmail.com>
2019-06-24 08:05:38 +0800

    add axios

---
+++ src/axios/index.js
@@ -0,0 +1,77 @@
+import axios from 'axios';
+import qs from 'qs';
+import router from '../router';
+
+const devUrl = 'http://localhost:8080';
+const prodUrl = 'http://www.baidu.com';
+
+const apiBaseUrl = process.env.NODE_ENV === 'production' ? prodUrl : devUrl;
+const Axios = axios.create({
+  baseURL: apiBaseUrl, // 因为我本地做了反向代理
+  timeout: 10000,
+  responseType: 'json',
+  withCredentials: true, // 是否允许带cookie这些
+  headers: {
+    'content-type': 'application/x-www-form-urlencoded'
+  }
+});
+
+Axios.interceptors.request.use(
+  config => {
+    // 在发送请求之前做某件事
+    if (config.method === 'post' || config.method === 'put' || config.method === 'delete') {
+      // 序列化
+      config.data = qs.stringify(config.data);
+    }
+
+    // 若是有做鉴权token , 就给头部带上token
+    if (localStorage.token) {
+      config.headers.Authorization = localStorage.token;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+// 返回状态判断(添加响应拦截器)
+Axios.interceptors.response.use(
+  res => {
+    // 对响应数据做些事
+    if (res.data && !res.data.success) {
+      //   Message({
+      //     //  饿了么的消息弹窗组件,类似toast
+      //     showClose: true,
+      //     message: res.data.message,
+      //     type: 'error'
+      //   })
+      // return Promise.reject(res.data)
+    }
+    return res.data;
+  },
+  error => {
+    // 用户登录的时候会拿到一个基础信息,比如用户名,token,过期时间戳
+    // 直接丢localStorage或者sessionStorage
+    if (error.response) {
+      switch (error.response.status) {
+      case 401:
+        router.push({
+          // 跳转到登录页面
+          path: '/login'
+        });
+        break;
+      case 404:
+        router.push({
+          path: '/404'
+        });
+        break;
+      }
+    }
+    return Promise.reject(error.response.data);
+    // 返回 response 里的错误信息
+    // let errorInfo = error.data.error ? error.data.error.message : error.data
+  }
+);
+
+export default Axios;
--- src/components/CatoryList/index.vue
+++ src/components/CatoryList/index.vue
@@ -2,8 +2,12 @@
   <div class="catory-list">
     <div class="title">{{ title }}</div>
     <div class="catory-list-item" v-for="item in dataList" v-bind:key="item.id">
-      <img src="@/assets/logo.png" alt="poster" />
-      <div>{{ item.name }}</div>
+      <img class="book-cover" src="@/assets/logo.png" alt="poster" />
+      <div class="book-info">
+        <div class="book-title">{{ item.name }}</div>
+        <div class="book-desc">{{ item.name }}</div>
+        <div class="book-viewer">{{ item.name }}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -23,9 +27,52 @@ export default {
   flex-direction: column;
   .title {
     text-align: left;
-    font-size: 0.2rem; /* 20/100 */
+    font-size: 0.16rem; /* 20/100 */
     color: #42b983;
     width: 100%;
+  }
+  .catory-list-item {
+    display: flex;
+    width: 100%;
+    padding: 0.1rem 0;
+    height: 0.8rem;
+    border-bottom: 1px solid #eeeeee;
+
+    .book-cover {
+      width: 0.625rem;
+      height: 0.8rem;
+    }
+  }
+  .book-info {
+    flex: 1;
+    margin-left: 0.2rem;
+    position: relative;
+    text-align: left;
+
+    .book-title {
+      color: #333;
+      font-size: 0.14rem;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      overflow: hidden;
+    }
+    .book-desc {
+      display: -webkit-box;
+      -webkit-line-clamp: 2;
+      -webkit-box-orient: vertical;
+      word-break: break-all;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      font-size: 0.12rem;
+      color: #999;
+      line-height: 0.2rem;
+    }
+
+    .book-viewer {
+      position: absolute;
+      left: 0;
+      bottom: 0;
+    }
   }
 }
 </style>
